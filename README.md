7960dc539ed7aa72ac0f8c1c787f0833b42fae66
Hassan Shaikh <shaikh.hassan01@outlook.com>
2022-04-18 08:55:30 -0400

    Used different methods to print dynamic outputs by using cmd line arguments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Portfolio-Generator
+
+#Description
+An interactive command-line application that processes user input using a third-party Node.js module. Uses string literals to dynamically generate markdown from the command line
+
+#Installation
+Use npm (Node Package Manager) to initialize a project and install and import Node.js modules
---
+++ app.js
@@ -0,0 +1,22 @@
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+// console.log(process);
+
+//this will remove the first two process of node and give an empty array starting at third index
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+  //Using for loop
+  for (let i = 0; i < profileDataArr.length; i++) {
+    console.log("Inside", profileDataArr[i]);
+  }
+  //using for each
+  profileDataArr.forEach((profileitem) => {
+    console.log("foreach", profileitem);
+  });
+
+  //using foreach without parentheses
+  profileDataArr.forEach((profileitem) => console.log("foreach2", profileitem));
+};
+printProfileData(profileDataArgs);
